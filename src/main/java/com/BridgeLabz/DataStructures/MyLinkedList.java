11e381c2df44f667c7f893b48c15227fb39d5859
mouna07 <mounamc261@gmail.com>
2021-02-24 21:03:24 +0530

    Added: Searching Data Nodes in linked list

---
+++ src/main/java/com/BridgeLabz/DataStructures/MyLinkedList.java
@@ -0,0 +1,74 @@
+package com.BridgeLabz.DataStructures;
+
+public class MyLinkedList<T> {
+    public INode<T> head;
+    public INode<T> tail;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    /* Add First */
+    public void addFirst(INode<T> newNode) {
+        if (this.tail == null) {
+            this.tail = newNode;
+        }
+        if (head == null) {
+            head = newNode;
+        } else {
+            INode<T> tempNode = this.head;
+            this.head = newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+
+    /* Add Last */
+    public void addLast(INode<T> newNode) {
+        if (this.head == null) {
+            this.head = newNode;
+        }
+        if (tail == null) {
+            tail = newNode;
+        } else {
+            this.tail.setNext(newNode);
+            tail = newNode;
+        }
+    }
+
+    /* Insert Node Between Nodes */
+    public void insertNode(INode<Integer> myFirstNode, INode<Integer> mySecondNode) {
+        mySecondNode.setNext(myFirstNode.getNext());
+        myFirstNode.setNext(mySecondNode);
+    }
+
+    /* Delete/Pop First Node */
+    public INode<T> popFirst() {
+        INode<T> tempNode = this.head;
+        this.head = this.head.getNext();
+        return tempNode;
+    }
+
+    /* Delete Pop Last Node */
+    public INode<T> popLast() {
+        INode<T> tempNode = head;
+        while (!(tempNode.getNext().equals(tail))) {
+            tempNode = tempNode.getNext();
+        }
+        this.tail = tempNode;
+        tempNode = tempNode.getNext();
+        return tempNode;
+    }
+
+    /* Search Node with given key */
+    public INode searchNode(T key) {
+        INode tempNode = this.head;
+        while (tempNode != null) {
+            if (tempNode.getData() == key) {
+                return tempNode;
+            }
+            tempNode = tempNode.getNext();
+        }
+        return null;
+    }
+}
---
+++ src/test/java/com/BridgeLabz/DataStructures/MyLinkedListTest.java
@@ -0,0 +1,93 @@
+package com.BridgeLabz.DataStructures;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyLinkedListTest {
+    /* Created Simple Linked List and Added to the First Position */
+    @Test
+    public void GivenThreeNumbersWhenAddedToLinkedListShouldAddedtoTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
+        myLinkedList.addFirst(myFirstNode);
+        myLinkedList.addFirst(mySecondNode);
+        myLinkedList.addFirst(myThirdNode);
+        boolean result = (myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode)
+                && myLinkedList.tail.equals(myFirstNode));
+        Assert.assertEquals(true, result);
+    }
+
+    /* Created Simple Linked List and Added to the Last Position */
+    @Test
+    public void GivenThreeNumbersWhenAddedToLinkedListShouldAddedtoBottom() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
+        myLinkedList.addLast(myFirstNode);
+        myLinkedList.addLast(mySecondNode);
+        myLinkedList.addLast(myThirdNode);
+        boolean result = (myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
+                && myLinkedList.tail.equals(myThirdNode));
+        Assert.assertEquals(true, result);
+    }
+
+    /* Insert a Node in Between the Nodes */
+    @Test
+    public void GivenANodeInsertedInBetweenResultingNodesReturnTrue() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
+        myLinkedList.addLast(myFirstNode);
+        myLinkedList.addLast(myThirdNode);
+        myLinkedList.insertNode(myFirstNode, mySecondNode);
+        boolean result = (myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
+                && myLinkedList.tail.equals(myThirdNode));
+        Assert.assertEquals(true, result);
+    }
+
+    /* Remove First Node */
+    @Test
+    public void PopNodeFromFrontShouldReturnTrue() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
+        myLinkedList.addFirst(myFirstNode);
+        myLinkedList.addFirst(mySecondNode);
+        myLinkedList.addFirst(myThirdNode);
+        INode<Integer> deletedNode = myLinkedList.popFirst();
+        Assert.assertEquals(deletedNode, myThirdNode);
+    }
+
+    /* Remove Last Node */
+    @Test
+    public void PopNodeFromLastShouldReturnTrue() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
+        myLinkedList.addFirst(myFirstNode);
+        myLinkedList.addFirst(mySecondNode);
+        myLinkedList.addFirst(myThirdNode);
+        INode<Integer> deletedNode = myLinkedList.popLast();
+        Assert.assertEquals(deletedNode, myFirstNode);
+    }
+
+    /* Search Node with given data */
+    @Test
+    public void SearchNodeForGivenDataIfFoundShouldReturnTrue() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
+        myLinkedList.addFirst(myFirstNode);
+        myLinkedList.addFirst(mySecondNode);
+        myLinkedList.addFirst(myThirdNode);
+        INode searchNode = myLinkedList.searchNode(30);
+        Assert.assertEquals(searchNode, mySecondNode);
+    }
+}
