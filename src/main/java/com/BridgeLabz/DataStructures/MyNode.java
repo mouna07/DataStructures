11b24a87f3a5b2cb3e53e8766e247335c1878628
mouna07 <mounamc261@gmail.com>
2021-02-24 20:54:14 +0530

    Added: Nodes

---
+++ src/main/java/com/BridgeLabz/DataStructures/MyNode.java
@@ -0,0 +1,33 @@
+package com.BridgeLabz.DataStructures;
+
+public class MyNode<T> {
+    private T data;
+    private MyNode next;
+    MyNode(){}
+
+    // Constructor with node fields
+    public MyNode(T key) {
+        this.setData(data);
+        this.setNext(null);
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+    public void printWelcomeMessage() {
+        System.out.println("Welcome to the Data Structure Problem");
+    }
+}
---
+++ src/test/java/com/BridgeLabz/DataStructures/MyNodeTest.java
@@ -0,0 +1,24 @@
+package com.BridgeLabz.DataStructures;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyNodeTest {
+    @Test
+    public void givenCallShouldPrintWelcomeMessage() {
+        MyNode myFirstNode = new MyNode();
+        myFirstNode.printWelcomeMessage();
+    }
+
+    @Test
+    public void GivenThreeNumbersWhenLinkedShouldPassLinkedListTest() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = (myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode));
+        Assert.assertTrue(result);
+    }
+
+}
