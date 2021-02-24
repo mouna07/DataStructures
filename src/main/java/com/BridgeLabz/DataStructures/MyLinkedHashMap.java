ed2b026ed1e5583b7b8e5171bae6c82fa194ed37
mouna07 <mounamc261@gmail.com>
2021-02-24 21:03:24 +0530

    Added: code for removing word from the hash map

---
+++ src/main/java/com/BridgeLabz/DataStructures/MyLinkedHashMap.java
@@ -0,0 +1,63 @@
+package com.BridgeLabz.DataStructures;
+
+import java.util.ArrayList;
+
+public class MyLinkedHashMap<T, V> {
+    private final int numBuckets;
+    ArrayList<MyLinkedList> myBucketArray;
+
+    // Constructor
+    public MyLinkedHashMap() {
+        numBuckets = 10;
+        myBucketArray = new ArrayList<>(numBuckets);
+        for (int i = 0; i < numBuckets; i++)
+            this.myBucketArray.add(null);
+    }
+
+    // Get bucket index for given key
+    private int getBucketIndex(T key) {
+        int hashCode = Math.abs(key.hashCode());
+        int index = hashCode % numBuckets;
+        return index;
+    }
+
+    // Get value given Key
+    public V get(T key) {
+        int index = this.getBucketIndex(key);
+        MyLinkedList myLinkedList = myBucketArray.get(index);
+        if (myLinkedList == null)
+            return null;
+        MyMapNode<T, V> myMapNode = (MyMapNode<T, V>) myLinkedList.searchNode((Comparable) key);
+        return (myMapNode == null) ? null : myMapNode.getValue();
+    }
+
+    // Add to linked hash map
+    public void add(T key, V value) {
+        int index = getBucketIndex(key);
+        MyLinkedList myLinkedList = myBucketArray.get(index);
+        if (myLinkedList == null) {
+            myLinkedList = new MyLinkedList();
+            myBucketArray.set(index, myLinkedList);
+        }
+        MyMapNode<T, V> myMapNode = (MyMapNode<T, V>) myLinkedList.searchNode((Comparable) key);
+        if (myMapNode == null) {
+            myMapNode = new MyMapNode<T, V>(key, value);
+            myLinkedList.addLast(myMapNode);
+        } else {
+            myMapNode.setValue(value);
+        }
+    }
+
+    // Remove the key-value pair for given key
+    public V remove(T key) {
+        int index = getBucketIndex(key);
+        MyLinkedList myLinkedList = myBucketArray.get(index);
+        MyMapNode<T, V> myMapNode = (MyMapNode<T, V>) myLinkedList.searchNode((Comparable) key);
+        if (myMapNode != null) {
+            V deletedValue = myMapNode.getValue();
+            myLinkedList.delete((Comparable) key);
+            return deletedValue;
+        } else
+            return null;
+    }
+}
