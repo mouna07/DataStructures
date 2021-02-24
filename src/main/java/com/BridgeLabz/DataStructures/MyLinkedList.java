2fb858d3220e5d63ebe7d147defa6f0eddd43153
mouna07 <mounamc261@gmail.com>
2021-02-24 21:03:24 +0530

    Added: Added nodes to the Last position

---
+++ src/main/java/com/BridgeLabz/DataStructures/MyLinkedList.java
@@ -0,0 +1,54 @@
+package com.BridgeLabz.DataStructures;
+
+public class MyLinkedList<T> {
+    public INode<T> head;
+    public INode<T> tail;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    /* Add First */
+    public void addFirst(INode newNode) {
+        if (this.tail == null) {
+            this.tail = newNode;
+        }
+        if (head == null) {
+            head = newNode;
+        } else {
+            INode tempNode = this.head;
+            this.head = newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+
+    /* Add Last */
+    public void addLast(INode newNode) {
+        if (this.head == null) {
+            this.head = newNode;
+        }
+        if (tail == null) {
+            tail = newNode;
+        } else {
+            this.tail.setNext(newNode);
+            tail = newNode;
+        }
+    }
+
+    public void printMyNodes() {
+        INode tempNode = this.head;
+        StringBuffer displayNode = new StringBuffer("My Nodes: ");
+        while (tempNode.getNext() != null) {
+            displayNode.append(tempNode.getData());
+            if (!tempNode.equals(this.tail)) {
+                displayNode.append("->");
+            }
+            tempNode = tempNode.getNext();
+        }
+        displayNode.append(tempNode.getData());
+        System.out.println(displayNode);
+
+    }
+
+}
---
+++ src/test/java/com/BridgeLabz/DataStructures/MyLinkedListTest.java
@@ -0,0 +1,38 @@
+package com.BridgeLabz.DataStructures;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyLinkedListTest {
+    /* Created Simple Linked List and Added to the First Position */
+    @Test
+    public void GivenThreeNumbersWhenAddedToLinkedListShouldAddedtoTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
+        myLinkedList.addFirst(myFirstNode);
+        myLinkedList.addFirst(mySecondNode);
+        myLinkedList.addFirst(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = (myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode)
+                && myLinkedList.tail.equals(myFirstNode));
+        Assert.assertEquals(true, result);
+    }
+
+    /* Created Simple Linked List and Added to the Last Position */
+    @Test
+    public void GivenThreeNumbersWhenAddedToLinkedListShouldAddedtoBottom() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
+        myLinkedList.addLast(myFirstNode);
+        myLinkedList.addLast(mySecondNode);
+        myLinkedList.addLast(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = (myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
+                && myLinkedList.tail.equals(myThirdNode));
+        Assert.assertEquals(true, result);
+    }
+}
