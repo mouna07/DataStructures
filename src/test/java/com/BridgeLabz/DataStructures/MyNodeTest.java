e2eece3171b5b7b0440742d3c75a39a5da475c6c
mouna07 <mounamc261@gmail.com>
2021-02-24 21:03:24 +0530

    Added: Insertion Between 2 Nodes

--- src/test/java/com/BridgeLabz/DataStructures/MyNodeTest.java
+++ src/test/java/com/BridgeLabz/DataStructures/MyNodeTest.java
@@ -2,6 +2,7 @@ package com.BridgeLabz.DataStructures;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 public class MyNodeTest {
     @Test
     public void givenCallShouldPrintWelcomeMessage() {
@@ -9,11 +10,13 @@ public class MyNodeTest {
         myFirstNode.printWelcomeMessage();
     }
 
+    /* Created Simple Linked List */
     @Test
     public void GivenThreeNumbersWhenLinkedShouldPassLinkedListTest() {
         MyNode<Integer> myFirstNode = new MyNode<>(56);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
         myFirstNode.setNext(mySecondNode);
         mySecondNode.setNext(myThirdNode);
         boolean result = (myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode));
